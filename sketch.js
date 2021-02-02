0789a2d5348e9f3f4899927c7d19483ce2a82d0e
SanskritiVairagade707 <74668144+sanskriti707@users.noreply.github.com>
2021-02-02 18:39:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,55 @@
-var database;
-var back_img;
-var gameState =0;
-var playerCount = 0;
-var allPlayers;
-var score =0;
-var player, form,game;
-var player1,player2;
-var players;
-var fruits;
-var fruitGroup;
-var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
-var player_img;
-var player1score =0;
-var player2score =0;
-var gameState =0;
-var playerCount =0;
+var hr,mn,sc;
+var hrAngle,mnAngle,scAngle;
 
-function preload(){
-  back_img = loadImage("images/jungle.jpg");
-  player_img = loadImage("images/basket2.png");
-  fruit1_img = loadImage("images/apple2.png");
-  fruit2_img = loadImage("images/banana2.png");
-  fruit3_img = loadImage("images/melon2.png");
-  fruit4_img = loadImage("images/orange2.png");
-  fruit5_img = loadImage("images/pineapple2.png");
-  fruitGroup = new Group();
-}
+
 function setup() {
-  createCanvas(1000, 600);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
-  
+  createCanvas(400,400);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(back_img);
+  background(0);
+  translate (200,200);
+  rotate (-90);
+  hr=hour();
+  mn=minute();
+  sc=second();
 
-  if (playerCount===2){
-    game.update(1);
+  scAngle=map(sc,0,60,0,360);
+  mnAngle=map(mn,0,60,0,360);
+  hrAngle=map(hr%12,0,12,0,360);
 
-  }
-  if(gameState===1){
-    clear ();
-    game.play();
-  }
-  if (gameState===2){
-    game.end();
-  }
+  push();
+  rotate(scAngle);
+  stroke ("red");
+  strokeWeight(7);
+  line (0,0,100,0);
+  pop ();
+
+  push();
+  rotate(mnAngle);
+  stroke ("green");
+  strokeWeight(7);
+  line (0,0,75,0);
+  pop ();
+
+  push();
+  rotate(hrAngle);
+  stroke ("blue");
+  strokeWeight(7);
+  line (0,0,50,0);
+  pop ();
+
+  stroke("yellow");
+  point (0,0);
+
+  strokeWeight(10);
+  stroke("red");
+  arc (0,0,300,300,0,scAngle);
+
+  stroke("green");
+  arc (0,0,280,280,0,mnAngle);
+
+  stroke("blue");
+  arc (0,0,260,260,0,hrAngle);
 }
